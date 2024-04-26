f29a5e10f608f681a3b8c56fc268a96b9805f2df
Joel Moyal <joelmoyal123@gmail.com>
2024-04-25 18:56:42 -0600

    adding option to use one button

--- projects/script.js
+++ projects/script.js
@@ -55,14 +55,19 @@ function showProjects(projects) {
         <div class="desc">
           <p>${project.desc}</p>
           <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>`
+        if (project.links.code){
+            projectsHTML+= `<a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>`
+            }
+        
+        projectsHTML += `
+        </div>
         </div>
       </div>
     </div>
-    </div>`
-    });
+    </div>`;
+});
+        
     projectsContainer.innerHTML = projectsHTML;
 
     // vanilla tilt.js
