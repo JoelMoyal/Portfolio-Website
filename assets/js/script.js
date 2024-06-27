47d27b90b3d3e91c251986c5cc5cab0c14422557
Joel Moyal <joelmoyal123@gmail.com>
2024-06-27 14:46:38 -0400

    Disable hover function projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -133,9 +133,9 @@ function showProjects(projects) {
     projectsContainer.innerHTML = projectHTML;
 
     // <!-- tilt js effect starts -->
-    VanillaTilt.init(document.querySelectorAll(".tilt"), {
-        max: 15,
-    });
+    // VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    //     max: 15,
+    // });
     // <!-- tilt js effect ends -->
 
     /* ===== SCROLL REVEAL ANIMATION ===== */
