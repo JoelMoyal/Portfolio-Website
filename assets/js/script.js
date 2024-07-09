8125270cb5d559d6dc44f0f1d82381d7a14c9326
Joel Moyal <joelmoyal123@gmail.com>
2024-07-08 23:57:03 -0400

    Updating typing text

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web design", "media Design", "Web development", "e commerce"],
+    strings: ["frontend developer", "backend developer", "web designer", "media Designer", "Web developer"],
     loop: true,
     typeSpeed: 40,
     backSpeed: 25,
