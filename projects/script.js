ade8d26d3787585eceba31e641ff83af735e341d
Naim Joël Moyal <joelmoyal123@gmail.com>
2026-03-22 17:40:24 -0700

    Simplify projects isotope setup

--- projects/script.js
+++ projects/script.js
@@ -74,38 +74,15 @@ function showProjects(projects) {
 
   projectsContainer.innerHTML = projectsHTML;
 
-  // vanilla tilt.js
-  // VanillaTilt.init(document.querySelectorAll(".tilt"), {
-  //     max: 20,
-  // });
-  // // vanilla tilt.js
-
-  // /* ===== SCROLL REVEAL ANIMATION ===== */
-  // const srtop = ScrollReveal({
-  //     origin: 'bottom',
-  //     distance: '80px',
-  //     duration: 1000,
-  //     reset: true
-  // });
-
-  // /* SCROLL PROJECTS */
-  // srtop.reveal('.work .box', { interval: 200 });
-
-  // isotope filter products
   var $grid = $(".box-container").isotope({
     itemSelector: ".grid-item",
     layoutMode: "fitRows",
-    masonry: {
-      columnWidth: 200,
-    },
   });
 
-  // filter items on button click
   $(".button-group").on("click", "button", function () {
     $(".button-group").find(".is-checked").removeClass("is-checked");
     $(this).addClass("is-checked");
-    var filterValue = $(this).attr("data-filter");
-    $grid.isotope({ filter: filterValue });
+    $grid.isotope({ filter: $(this).attr("data-filter") });
   });
 }
 
